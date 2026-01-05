006148038f14d1c0fbc4aa0effbf86f801eefa11
gtkrshnaaa <gtkrshnaaa@gmail.com>
2026-01-05 18:41:43 +0700

    feat: implement FAQ models with relationships

---
+++ app/Models/Faq.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Faq extends Model
+{
+    protected $fillable = ['faq_category_id', 'question', 'answer', 'order'];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(FaqCategory::class, 'faq_category_id');
+    }
+}
